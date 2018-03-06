8b37c6b0698214e41b79a4817e79743ad933b70f
rhysr <rhys@mega-city-one.co.uk>
2018-03-06 19:34:15 +0000

    Pass name of socket process to frontend

--- sockets/start.js
+++ sockets/start.js
@@ -5,9 +5,15 @@ const process = require('process');
 const socketio = require('socket.io');
 const server = http.createServer();
 
+if ('undefined' === typeof process.env.LABEL) {
+    console.error('No LABEL environment variable set');
+    process.exit(1);
+}
+const label = process.env.LABEL;
+
 const io = socketio(server);
 io.on('connection', (socket) => {
-    socket.emit('news', { pid: process.pid });
+    socket.emit('news', { socket_name: label });
 });
 
 server.listen(port, (err) => {
